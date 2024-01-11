7dfa443588bf742571e62352f9c366588cf43131
iam8uman <iam8uman@gmail.com>
2024-01-11 21:27:37 +0545

    Zustand setup prerequisits

---
+++ zustand/src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello i am about page
+    </div>
+  )
+}
+
+export default page
---
+++ zustand/src/app/home/mainhome/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hello i am main home page
+    </div>
+  )
+}
+
+export default page
