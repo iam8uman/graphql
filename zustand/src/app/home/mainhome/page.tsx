358127839931d376ad904fc81d49df90492da980
iam8uman <iam8uman@gmail.com>
2024-01-11 22:11:38 +0545

    zustand working perfeclty fine

--- zustand/src/app/home/mainhome/page.tsx
+++ zustand/src/app/home/mainhome/page.tsx
@@ -1,11 +1,17 @@
+"use client"
 import React from 'react'
+import {useCounterStore} from '@/store/CounterState'
 
-const page = () => {
+const Page = () => {
+    const { count, increment, decrement } = useCounterStore();
   return (
     <div>
       hello i am main home page
+      <h1>Count: {count+1}</h1>
+      <button onClick={increment} className='button bg-purple-500 rounded-lg text-white m-3 p-3'>Increment</button>
+      <button onClick={decrement} className='button bg-purple-500 rounded-lg text-white m-3 p-3'>Decrement</button>
     </div>
   )
 }
 
-export default page
+export default Page
--- zustand/src/app/home/page.tsx
+++ zustand/src/app/home/page.tsx
@@ -3,15 +3,15 @@
 import React from 'react'
 import {useCounterStore} from '@/store/CounterState'
 
-const page = () => {
+const Page = () => {
     const { count, increment, decrement } = useCounterStore();
   return (
     <div>
-      <h1>Count: {count}</h1>
+      <h1>Count: {count+1}</h1>
       <button onClick={increment} className='button bg-purple-500 rounded-lg text-white m-3 p-3'>Increment</button>
       <button onClick={decrement} className='button bg-purple-500 rounded-lg text-white m-3 p-3'>Decrement</button>
     </div>
   )
 }
 
-export default page
+export default Page
